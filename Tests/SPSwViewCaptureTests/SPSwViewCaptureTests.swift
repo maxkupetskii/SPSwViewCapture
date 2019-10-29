05c45f573a448b347ce92b5d56de1dba0ed2a558
Max Kupetskii <maksim_kupetskii@epam.com>
2019-10-29 15:24:12 +0300

    tests fixed

--- Tests/SPSwViewCaptureTests/SPSwViewCaptureTests.swift
+++ Tests/SPSwViewCaptureTests/SPSwViewCaptureTests.swift
@@ -6,7 +6,6 @@ final class SPSwViewCaptureTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SPSwViewCapture().text, "Hello, World!")
     }
 
     static var allTests = [
